f454cc5ba66a9986cc6938611b0ffc253f122704
kubat <kubat.aiupov@gmail.com>
2025-04-29 21:37:52 +0200

    add util functions to add 2 and 3 points

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ function addOne() {
   countHome += 1;
   homeScoreEl.innerHTML = countHome;
 }
+
+function addTwo() {
+  countHome += 2
+  homeScoreEl.innerHTML = countHome
+}
+
+
+function addThree() {
+  countHome += 3
+  homeScoreEl.innerHTML = countHome
+}
