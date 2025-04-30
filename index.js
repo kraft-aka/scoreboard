acab28792fc0f846b252fd8abc75523cf8e3f2cf
kubat <kubat.aiupov@gmail.com>
2025-04-30 22:00:25 +0200

    add reset btn, add func for guest

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-const homeBtns = document.getElementById("add-points-btns-home");
 const homeScoreEl = document.getElementById("home-score");
-const addOneBtn = document.getElementById("home-add-one");
+const guestScoreEl = document.getElementById("guest-score");
 let countHome = 0;
+let countGuest = 0;
 
-function addOne() {
+function addOneH() {
   countHome += 1;
   homeScoreEl.innerHTML = countHome;
 }
 
-function addTwo() {
-  countHome += 2
-  homeScoreEl.innerHTML = countHome
+function addTwoH() {
+  countHome += 2;
+  homeScoreEl.innerHTML = countHome;
 }
 
+function addThreeH() {
+  countHome += 3;
+  homeScoreEl.innerHTML = countHome;
+}
 
-function addThree() {
-  countHome += 3
-  homeScoreEl.innerHTML = countHome
+function addOneG() {
+  countGuest += 1;
+  guestScoreEl.innerHTML = countGuest;
+}
+
+function addTwoG() {
+  countGuest += 2;
+  guestScoreEl.innerHTML = countGuest;
+}
+
+function addThreeG() {
+  countGuest += 3;
+  guestScoreEl.innerHTML = countGuest;
+}
+
+function reset() {
+  countHome = 0;
+  countGuest = 0;
+  homeScoreEl.innerHTML = countHome;
+  guestScoreEl.innerHTML = countGuest;
 }
